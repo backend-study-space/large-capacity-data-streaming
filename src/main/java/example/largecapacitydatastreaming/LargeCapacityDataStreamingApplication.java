83dd31cdb2ecbb0b73c3de85e120b0e0e2a0fc7e
2tsumo-hitori <audghks1996@naver.com>
2024-04-02 22:36:24 +0900

    initial commit

---
+++ src/main/java/example/largecapacitydatastreaming/LargeCapacityDataStreamingApplication.java
@@ -0,0 +1,13 @@
+package example.largecapacitydatastreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargeCapacityDataStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LargeCapacityDataStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/largecapacitydatastreaming/LargeCapacityDataStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package example.largecapacitydatastreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargeCapacityDataStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
